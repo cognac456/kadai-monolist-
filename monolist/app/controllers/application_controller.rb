1ee44ddcc415284fe5d708edbfc4e7ea961210ca
cognac <femt456@gmail.com>
2018-06-02 03:10:37 +0000

    user login

--- monolist/app/controllers/application_controller.rb
+++ monolist/app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  before_action :require_user_logged_in, only: [:show]
+  
+  
+  include SessionsHelper
+  
+  def require_user_logged_in
+    if !logged_in?
+      redirect_to login_path
+    end
+  end
+  
 end
