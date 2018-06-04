82ebc6e4249bfacec981e37fd8fd95f5b3db309a
cognac <femt456@gmail.com>
2018-06-04 14:57:30 +0000

    kadai_monolist_1

--- monolist/app/controllers/items_controller.rb
+++ monolist/app/controllers/items_controller.rb
@@ -3,30 +3,27 @@ class ItemsController < ApplicationController
     @items=[]
     
     @keyword= params[:keyword]
-    if @keyword.present?
-      results= RakutenWebService::Ichiba::Item.search({
+    if @keyword
+      @results= RakutenWebService::Ichiba::Item.search({
+#      results= RakutenWebService::Ichiba::Item.search({
         keyword: @keyword,
         imageFlag: 1,
         hits: 20,
       })
-      results.each do |result|
-        item= Item.new(read(result))
+     @results.each do |result|
+#      results.each do |result|
+#        item= Item.new(read(result))
+        item = Item.find_or_initialize_by(read(result)) 
         @items << item
       end
     end
-    
-    def read(result)
-      code= result["itemCode"]
-      name= result["itemName"]
-      url= result["itemUrl"]
-      image_url= result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
-    
-      return{
-        code: code,
-        name: name,
-        url: url,
-        image_url: image_url,
-      }
-    end
   end
+  
+  def show
+    @item = Item.find(params[:id])
+    @want_users= @item.want_users
+    @have_users= @item.have_users
+  end
+    
+  
 end
--- monolist/config/routes.rb
+++ monolist/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
   
-  resources :items, only: [:new]
+  resources :items, only: [:new, :show]
+  resources :ownerships, only: [:create, :destroy]
   
 end
